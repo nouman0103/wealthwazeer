e6dc1dbafdcff0c34cb4286bc1d7cde95d275362
abdullah134 <abdullahaqeel184@gmail.com>
2024-03-25 10:18:44 +0500

    Fix Fastapi autoreload and add pycahce to ignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union,Optional
 
 from fastapi import FastAPI
 
@@ -7,9 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "Test" : "World1234"
+    }
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
