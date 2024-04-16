857aa3175a9bf74ef97b93b40b43ab555ba37ca9
imahrukh <me.mahrukhch@gmail.com>
2024-04-16 15:11:47 +0500

    Transactions card on Dashboarrd

--- frontend/app/user/dashboard/page.tsx
+++ frontend/app/user/dashboard/page.tsx
@@ -1,4 +1,33 @@
 import React from 'react';
+import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded';
+import ArrowDownwardRoundedIcon from '@mui/icons-material/ArrowDownwardRounded';
+const TransactionCards: React.FC<{title: string, date: string, value: string, isArrowUp: boolean}>=({title,date,value, isArrowUp}) =>{
+  return (
+    <div className='flex items-center px-8 bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-3xl border border-opacity-5 border-white'>
+      {isArrowUp ? (
+        <ArrowUpwardRoundedIcon className='text-red-500 text-opacity-70' />
+      ) : (
+        <ArrowDownwardRoundedIcon className='text-green-500 text-opacity-70' />
+      )}
+        <div className='flex-grow flex flex-col gap-1 ml-4'>
+          <text className='text-xl font-medium text-white  self-start'>
+           {title}
+          </text>
+          <text className='text-xs font-medium text-white text-opacity-70 self-start'>
+            {date}
+          </text>
+        </div>
+        <div className='flex items-left'>
+          <text className='text-md font-medium text-white text-opacity-70 ml-3'>
+            Rs
+          </text>
+          <text className='text-2xl font-medium text-white  ml-1'>
+            {value}
+          </text>
+        </div>
+     </div>
+  )
+}
 
 const DashCard: React.FC<{ title: string, value: string }> = ({ title, value }) => {
   return (
@@ -35,19 +64,27 @@ export default function Home() {
   return (
     <div className="p-10">
       <div className='flex gap-10'>
-        <DashCard title='Net Savings' value='3455'/>
-        <DashCard title='Income' value='3455'/>
-        <DashCard title='Total Spent' value='3455'/>
-        <div className='bg-blue-600 w-10 h-10'>
-          <text>
-            this is a test
-          </text>
+
+        <DashCard title='Total Spent' value='3455' />
+
+        <div className='p-10'>
+          <div className='bg-glassmorphic-gradient backdrop backdrop-blur-lg shadow-glassmorphic rounded-3xl border border-opacity-5 border-white p-8 flex flex-col gap-3'>
+            <div className='flex justify-between'>
+              <text className='text-lg font-medium text-white text-opacity-70'>
+               Transactions
+              </text>
+              <text className='text-xs font-medium text-rose-400'>
+              See all
+              </text>
+            </div> 
+
+            <TransactionCards title ='Spotify Premium' date='11:00 am, 15/04' value = '400'isArrowUp ={true} />
+            <TransactionCards title ='Youtube Premium' date='11:00 am, 15/04' value = '320' isArrowUp={true}/>
+            <TransactionCards title ='Amazon.com Order' date='11:00 am, 15/04' value = '1200'isArrowUp={true}/>
+            <TransactionCards title ='Payoneer' date='11:00 am, 15/04' value = '32000'isArrowUp={false}/>
+          </div>
         </div>
       </div>
-
-
-
-
     </div>
   );
 }
