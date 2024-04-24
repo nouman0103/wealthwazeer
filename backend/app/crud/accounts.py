91c2dbf0d4f7bc62409694c512423e0d38bc9381
abdullah134 <abdullahaqeel184@gmail.com>
2024-04-24 17:18:45 +0500

    Create Account API

--- backend/app/crud/accounts.py
+++ backend/app/crud/accounts.py
@@ -13,4 +13,9 @@ def get_account_detail(db: Session, user_id: int) -> schemas.AccountDetail:
         
     return schemas.AccountDetail(accounts=accounts)
     
-    
+def create_account(db: Session, account: schemas.AccountCreate, user_id: int) -> schemas.Account:
+    db_account = models.Account(name=account.name, account_type=account.account_type, user_id=user_id)
+    db.add(db_account)
+    db.commit()
+    db.refresh(db_account)
+    return schemas.Account(name=db_account.name, account_type=db_account.account_type, balance=0.0)
