5d2a1340aa2f3f9632967bbffa1d0ac1036b7850
nouman0103 <nouman0103@gmail.com>
2024-04-25 17:45:17 +0500

    Fix some stuff in contacts

--- frontend/app/user/contacts/page.tsx
+++ frontend/app/user/contacts/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Avatar, IconButton, Divider} from '@mui/material';
+import { List, Avatar, IconButton, Divider } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AddIcon from '@mui/icons-material/Add';
@@ -30,7 +30,7 @@ const SearchButton: React.FC<SearchButtonProps> = ({ title }) => {
       </IconButton>
       <span className="text-white text-sm text-opacity-70">
         {title}
-        </span>
+      </span>
     </div>
   );
 };
@@ -42,7 +42,7 @@ const AddButton: React.FC<AddButtonProps> = ({ title }) => {
         <IconButton size="small">
           <AddIcon style={{ fontSize: '2rem', color: 'white', opacity: 0.7, marginRight: '0.5rem', marginTop: 'auto', marginBottom: 'auto' }} />
           <span className="text-white text-sm text-opacity-70 my-auto">{
-          title}
+            title}
           </span>
         </IconButton>
       </div>
@@ -58,10 +58,10 @@ const Requests: React.FC<Friend> = ({ name, email }) => {
   };
 
   const avatarStyle = {
-    backgroundColor: getAvatarColor(name[0]), 
-    color: '#fff', 
+    backgroundColor: getAvatarColor(name[0]),
+    color: '#fff',
     marginRight: '20px',
-    borderRadius: '50%' 
+    borderRadius: '50%'
   };
 
   return (
@@ -79,11 +79,11 @@ const Requests: React.FC<Friend> = ({ name, email }) => {
           </div>
         </div>
         <div className="options flex items-center">
-          <IconButton style={{ backgroundColor: 'transparent', padding:4 }}>
-            <CloseIcon style={{ color: '#FF0000', fontSize:18 }} />
+          <IconButton style={{ backgroundColor: 'transparent', padding: 4 }}>
+            <CloseIcon style={{ color: '#FF0000', fontSize: 18 }} />
           </IconButton>
-          <IconButton style={{ backgroundColor: 'transparent', padding:4 }}>
-            <DoneIcon style={{ color: '#6EE7B7',fontSize:18 }} />
+          <IconButton style={{ backgroundColor: 'transparent', padding: 4 }}>
+            <DoneIcon style={{ color: '#6EE7B7', fontSize: 18 }} />
           </IconButton>
         </div>
       </div>
@@ -99,21 +99,21 @@ const ContactItem: React.FC<Contact> = ({ name }) => {
   };
 
   const avatarStyle = {
-    backgroundColor: getAvatarColor(name[0]), 
-    color: '#fff', 
+    backgroundColor: getAvatarColor(name[0]),
+    color: '#fff',
     marginRight: '20px',
-    borderRadius: '50%' 
+    borderRadius: '50%'
   };
 
   return (
-    <div className="contact-item bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-xl border-opacity-5 border-white p-2 mb-4">
+    <div className="contact-item bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-xl border border-opacity-5 border-white p-2 mb-4">
       <div className="contact-content flex items-center justify-between">
         <Avatar style={avatarStyle}>
           {name[0]}
-          </Avatar>
+        </Avatar>
         <div className="name text-white text-sm flex-grow mr-40">
           {name}
-          </div>
+        </div>
         <div className="options">
           <IconButton>
             <MoreVertIcon />
@@ -132,30 +132,30 @@ const FriendItem: React.FC<Friend> = ({ name, email }) => {
   };
 
   const avatarStyle = {
-    backgroundColor: getAvatarColor(name[0]), 
-    color: '#fff', 
+    backgroundColor: getAvatarColor(name[0]),
+    color: '#fff',
     marginRight: '20px',
-    borderRadius: '50%' 
+    borderRadius: '50%'
   };
 
   return (
-    <div className="friend-item bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-xl border-opacity-5 border-white p-2 mb-2">
+    <div className="friend-item bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-xl border border-opacity-5 border-white p-2 mb-2">
       <div className='contact-content flex items-center justify'>
-        <Avatar style = {avatarStyle}>
+        <Avatar style={avatarStyle}>
           {name[0]}
         </Avatar>
-          <div className="name text-white text-sm flex-grow mr-20">
-            {name}
-          </div>
-          <div className="text-white text-sm  mr-20">
-            {email}
-          </div >
-      <div className="options">
-        <IconButton>
-          <MoreVertIcon />
-        </IconButton>
+        <div className="name text-white text-sm flex-grow mr-20">
+          {name}
+        </div>
+        <div className="text-white text-sm  mr-20">
+          {email}
+        </div >
+        <div className="options">
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
@@ -175,49 +175,48 @@ export default function Home() {
     { name: "John Doe", email: "john@example.com" },
     { name: "Jane Doe", email: "jane@example.com" },
     { name: "June Doe", email: "june@example.com" }
-   
+
   ];
 
   return (
-  
+
     <div className="p-8 flex flex-col gap-5 flex-grow overflow-hidden mb-1">
       <div className="flex">
-        <div className="text-2xl font-semibold text-white my-auto mr-40">
+        <div className="text-2xl font-medium text-white my-auto mr-40">
           Contacts
         </div>
-        <div className="text-2xl  ml-40 font-semibold text-white my-auto">
-        Partners
+        <div className="text-2xl  ml-40 font-medium text-white my-auto">
+          Friends
+        </div>
       </div>
-      </div>
-      <Divider orientation="vertical" flexItem />
-    <div className="flex items-center space-x-8">
-      <AddButton title="Add Account" />
-      <div className="ml-4"> 
-      <SearchButton title = "Search Contacts" />
-      </div>
-    </div> 
-      
+
       <div className="flex gap-5">
-        <List>
-          {contacts.map((contact, index) => (
-            <ContactItem key={index} {...contact} />
-          ))}
-        </List>
-  
+        <div>
+          <div className="flex justify-between mb-3">
+            <AddButton title="Add Account" />
+            <SearchButton title="Search Contacts" />
+          </div>
+          <List>
+            {contacts.map((contact, index) => (
+              <ContactItem key={index} {...contact} />
+            ))}
+          </List>
+        </div>
+
         <Divider orientation="vertical" flexItem />
         <div>
-        <List>
-          {friends.map((friend, index) => (
-            <Requests key={index} {...friend} />
-          ))}
-        </List>
+          <List>
+            {friends.map((friend, index) => (
+              <Requests key={index} {...friend} />
+            ))}
+          </List>
         </div>
         <div>
-        <List>
-          {friends.map((friend, index) => (
-            <FriendItem key={index} {...friend} />
-          ))}
-        </List>
+          <List>
+            {friends.map((friend, index) => (
+              <FriendItem key={index} {...friend} />
+            ))}
+          </List>
         </div>
       </div>
     </div>
