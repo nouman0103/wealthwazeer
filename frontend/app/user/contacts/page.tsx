dbee6a27558c8bf170368edb5bc41c5c17d45324
imahrukh <me.mahrukhch@gmail.com>
2024-04-25 20:47:45 +0500

    Added Search and Add buttons in Contacts

--- frontend/app/user/contacts/page.tsx
+++ frontend/app/user/contacts/page.tsx
@@ -4,6 +4,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AddIcon from '@mui/icons-material/Add';
 import { Close as CloseIcon, Done as DoneIcon } from '@mui/icons-material';
+import { GlassmorphicButton } from '@/components/buttons';
 
 interface Contact {
   name: string;
@@ -50,7 +51,7 @@ const AddButton: React.FC<AddButtonProps> = ({ title }) => {
   );
 };
 
-const Requests: React.FC<Friend> = ({ name, email }) => {
+const PendingRequests: React.FC<Friend> = ({ name, email }) => {
   const getAvatarColor = (initial: string): string => {
     const colors = ['#F87171', '#FBBF24', '#60A5FA', '#34D399', '#A78BFA', '#F472B6', '#6EE7B7', '#93C5FD', '#FCD34D', '#EDE9FE'];
     const charCode = initial.charCodeAt(0) % colors.length;
@@ -70,14 +71,13 @@ const Requests: React.FC<Friend> = ({ name, email }) => {
         <Avatar style={avatarStyle}>
           {name[0]}
         </Avatar>
-        <div className="details flex-grow">
-          <div className="name text-white text-sm">
+          <div className="name text-white text-sm  flex-grow">
             {name}
           </div>
-          <div className="email text-white text-sm">
+          <div className=" text-white text-sm flex-grow">
             {email}
           </div>
-        </div>
+        
         <div className="options flex items-center">
           <IconButton style={{ backgroundColor: 'transparent', padding: 4 }}>
             <CloseIcon style={{ color: '#FF0000', fontSize: 18 }} />
@@ -91,6 +91,42 @@ const Requests: React.FC<Friend> = ({ name, email }) => {
   );
 };
 
+const SentRequests: React.FC<Friend> = ({ name, email }) => {
+  const getAvatarColor = (initial: string): string => {
+    const colors = ['#F87171', '#FBBF24', '#60A5FA', '#34D399', '#A78BFA', '#F472B6', '#6EE7B7', '#93C5FD', '#FCD34D', '#EDE9FE'];
+    const charCode = initial.charCodeAt(0) % colors.length;
+    return colors[charCode];
+  };
+
+  const avatarStyle = {
+    backgroundColor: getAvatarColor(name[0]),
+    color: '#fff',
+    marginRight: '20px',
+    borderRadius: '50%'
+  };
+
+  return (
+    <div className="friend-item bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-xl border-opacity-5 border-white p-2 mb-4">
+      <div className='contact-content flex items-center justify-between'>
+        <Avatar style={avatarStyle}>
+          {name[0]}
+        </Avatar>
+          <div className="name text-white text-sm  flex-grow">
+            {name}
+          </div>
+          <div className=" text-white text-sm flex-grow">
+            {email}
+          </div>
+        
+        <div className="options flex items-center">
+          <IconButton style={{ backgroundColor: 'transparent', padding: 4 }}>
+            <CloseIcon style={{ color: '#FF0000', fontSize: 18 }} />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+};
 const ContactItem: React.FC<Contact> = ({ name }) => {
   const getAvatarColor = (initial: string): string => {
     const colors = ['#F87171', '#FBBF24', '#60A5FA', '#34D399', '#A78BFA', '#F472B6', '#6EE7B7', '#93C5FD', '#FCD34D', '#EDE9FE'];
@@ -144,10 +180,10 @@ const FriendItem: React.FC<Friend> = ({ name, email }) => {
         <Avatar style={avatarStyle}>
           {name[0]}
         </Avatar>
-        <div className="name text-white text-sm flex-grow mr-20">
+        <div className="name text-white text-sm flex-grow">
           {name}
         </div>
-        <div className="text-white text-sm  mr-20">
+        <div className="text-white text-sm  flex-grow ">
           {email}
         </div >
         <div className="options">
@@ -179,7 +215,6 @@ export default function Home() {
   ];
 
   return (
-
     <div className="p-8 flex flex-col gap-5 flex-grow overflow-hidden mb-1">
       <div className="flex">
         <div className="text-2xl font-medium text-white my-auto mr-40">
@@ -196,6 +231,7 @@ export default function Home() {
             <AddButton title="Add Account" />
             <SearchButton title="Search Contacts" />
           </div>
+
           <List>
             {contacts.map((contact, index) => (
               <ContactItem key={index} {...contact} />
@@ -204,17 +240,36 @@ export default function Home() {
         </div>
 
         <Divider orientation="vertical" flexItem />
-        <div className='flex flex-col flex-grow'>
-          <List>
-            {friends.map((friend, index) => (
-              <Requests key={index} {...friend} />
-            ))}
-          </List>
+
+        <div className ="flex-grow">
+        <div className="friend-item bg-glassmorphic-gradient backdrop-blur-lg shadow-glassmorphic rounded-xl border border-opacity-5 border-white flex-grow p-4 mb-2">
+          <div className="flex flex-col mb-3">
+            <AddButton title="Add friend" />
+          </div>
+        
+          <div className="flex justify-end mb-3"> 
+            <SearchButton title="Search in requests" /> 
+          </div>
+
+          <div className='flex flex-col flex-grow'>
+            <List>
+              {friends.map((friend, index) => (
+                <SentRequests key={index} {...friend} />
+              ))}
+            </List>
+          </div>
+          </div>
+
+          <div className="flex justify-end mb-3"> 
+            <SearchButton title="Search for friends" /> 
+          </div>
+          <div className='flex flex-col flex-grow'>
           <List>
             {friends.map((friend, index) => (
               <FriendItem key={index} {...friend} />
             ))}
           </List>
+          </div>
         </div>
       </div>
     </div>
