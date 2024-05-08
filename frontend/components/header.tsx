aa1e3c31603215d5f48ed86892a5474017ef4d64
imahrukh <me.mahrukhch@gmail.com>
2024-05-08 14:31:44 +0500

    chore: Update header component with About section and developer names

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,10 +1,22 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Logo_BlackOutline from "../assets/Logo_BlackOutline.svg";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Modal, Button } from "@mui/material";
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 const Header: React.FC = () => {
@@ -18,7 +30,7 @@ const Header: React.FC = () => {
   const handleCloseModal = () => {
     setOpenModal(false);
   };
-  
+
   return (
     <div>
       <header className="flex fixed z-30 w-full justify-around gap-16 p-2  bg-glassmorphic-gradient border-b border-white border-opacity-10 backdrop-blur-lg shadow-glassmorphic">
@@ -31,7 +43,7 @@ const Header: React.FC = () => {
         <nav className="mr-auto my-auto">
           <ul className="flex gap-7 text-lg text-opacity-80 text-white font-medium">
             <li>
-            <Link href="#" onClick={handleOpenModal}>About</Link>
+              <Link href="#" onClick={handleOpenModal}>About</Link>
             </li>
             <li>
               {pathname === "/signup" ? (
@@ -54,13 +66,22 @@ const Header: React.FC = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <div className="bg-glassmorphic-gradient p-4 rounded-3xl border border-opacity-5 border-white shadow-glassmorphic w-96 mx-auto mt-24">
+        <div className="bg-glassmorphic-gradient p-4 backdrop-blur-lg rounded-3xl border border-opacity-5 border-white shadow-glassmorphic w-96 mx-auto mt-24">
           <h2 id="modal-modal-title" className="text-3xl font-semibold mb-4">
             About WealthWazeer
           </h2>
           <p id="modal-modal-description" className="text-xl text-white">
-            This is some text about WealthWazeer.
-          </p>  
+            Revolutionizing personal finance with streamlined registration, customizable profiles, and comprehensive tracking for expenses, savings goals, and loans. Empowering users with seamless connectivity and personalized insights.
+          </p>
+          <p id="modal-modal-description" className="text-xl text-white">
+            Developers:
+          </p>
+          <ul className="ml-3">
+            <li>Shivam Kumar</li>
+            <li>Shubham Kumar</li>
+            <li>Shubham Kumar</li>
+            <li>Shubham Kumar</li>
+          </ul>
         </div>
       </Modal>
     </div>
