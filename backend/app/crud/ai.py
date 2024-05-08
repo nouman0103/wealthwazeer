9ae3266b0eef29275515a839a6070e5c52fef96a
nouman0103 <nouman0103@gmail.com>
2024-05-08 15:29:31 +0500

    update ai prompt

--- backend/app/crud/ai.py
+++ backend/app/crud/ai.py
@@ -15,8 +15,8 @@ model = genai.GenerativeModel('gemini-pro')
 
 async def get_tip_of_the_day(user_id:int,db:Session):
     personal = crud.get_detail_income_expense_data(db,user_id)
-    result = await model.generate_content_async(f"Give me a financing tip based on the data below. Mention specific account name for recommendation. Keep it short. Currency: RS. \n\n {json.dumps(personal.model_dump_json())}")
+    result = await model.generate_content_async(f"Give me a financing tip based on the data below. Mention specific account name for recommendation. Keep it short. Currency: RS. Income accounts are the current amount received from the bank account. Expense accounts contains the account's name with the amount spent. Bank and Cash accounts are the current balance in bank or cash. \n\n {json.dumps(personal.model_dump_json())}")
     try:
-        return result.text
+        return "AI Recommendation: " + result.text
     except Exception as e:
         raise HTTPException(status_code=500,detail="AI model failed to generate response")
